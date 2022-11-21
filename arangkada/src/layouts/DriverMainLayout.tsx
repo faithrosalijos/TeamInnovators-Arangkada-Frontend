8fe633b4d6bdaf7ef09f245740199da30d660f44
Faith Rosalijos <faithrosalijos@gmail.com>
2022-11-21 17:42:19 +0800

    Add main layout

---
+++ arangkada/src/layouts/DriverMainLayout.tsx
@@ -0,0 +1,39 @@
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import { DrawerHeader } from '../styles/NavbarStyles';
+import Navbar from '../components/Navbar';
+import Topbar from '../components/Topbar';
+import VehicleRentals from '../pages/VehicleRentals';
+
+
+const DriverMainLayout = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+
+      <Topbar open={open} handleDrawerOpen={handleDrawerOpen} />
+
+      <Navbar user="Driver" open={open} handleDrawerClose={handleDrawerClose} />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3, padding: "0 32px" }}>
+        <DrawerHeader />
+        {/* Your driver pages goes here. */}
+
+        {/* Comment out if you won't use */}
+        <VehicleRentals />
+        
+      </Box>
+    </Box>
+  );
+}
+
+export default DriverMainLayout;
