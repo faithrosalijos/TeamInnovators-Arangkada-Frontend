2eb25fd62230d63cd93abbf9cca08fedee8cc7e3
Mariel Genodiala <118412577+marielgenodiala@users.noreply.github.com>
2022-11-22 15:19:45 +0800

    Add MyVehicle pages

---
+++ arangkada/src/pages/MyVehiclePage/AddVehiclePage.tsx
@@ -0,0 +1,21 @@
+import { Box, Divider } from "@mui/material";
+import { useState } from "react";
+import Footer from "../../components/Footer";
+import AddVehicle from "../../components/MyVehicleComp/AddVehicle";
+import PageHeader from "../../components/PageHeader";
+
+
+
+
+const MyVehicles = () => {
+ 
+  return ( 
+    <Box sx={{ padding: "12px 0 0" }}>
+      <PageHeader title="Add Vehicle"/>
+      <AddVehicle/>
+      <Divider/>
+      <Footer name="Mariel Genodiala" course="BSIT" section="G3"/>
+    </Box>
+   );
+}
+export default MyVehicles;
