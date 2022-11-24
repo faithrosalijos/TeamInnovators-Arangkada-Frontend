28ac7972666b660a966b4d044b598b18f8207eeb
Faith Rosalijos <faithrosalijos@gmail.com>
2022-11-24 20:18:41 +0800

    Change location to route

--- arangkada/src/components/faith/VehicleFilterForm.tsx
+++ arangkada/src/components/faith/VehicleFilterForm.tsx
@@ -5,14 +5,14 @@ import PersonIcon from '@mui/icons-material/Person';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 type VehicleFilterFormProps = {
-  handleFilterSubmit: (businessName: string, operatorName: string, location: string) => void,
+  handleFilterSubmit: (businessName: string, operatorName: string, route: string) => void,
   handleFilterClear: () => void,
 }
 
 const VehicleFilterForm = ({ handleFilterSubmit, handleFilterClear }: VehicleFilterFormProps) => {
   const [businessName, setBusinessName] = useState("");
   const [operatorName, setOperatorName] = useState("");
-  const [location, setLocation] = useState("");
+  const [route, setRoute] = useState("");
 
   const handleBusinessNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setBusinessName(event.target.value);
@@ -23,22 +23,22 @@ const VehicleFilterForm = ({ handleFilterSubmit, handleFilterClear }: VehicleFil
   }
 
   const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLocation(event.target.value);
+    setRoute(event.target.value);
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    handleFilterSubmit(businessName, operatorName, location);
+    handleFilterSubmit(businessName, operatorName, route);
   }
 
   const handleClear = (event: React.MouseEvent) => {
     setBusinessName("");
     setOperatorName("");
-    setLocation("");
+    setRoute("");
     handleFilterClear();
   }
 
-  return ( 
+  return (
     <Grid container spacing={2} onSubmit={handleSubmit} component="form" sx={{ marginTop: "0", padding: "12px 0" }}>
       <Grid item xs={12} md={3}>
         <TextField onChange={handleBusinessNameChange} value={businessName} label="Search by Business Name" size="small" fullWidth InputProps={{ startAdornment: (<InputAdornment position="start"> <SearchIcon /> </InputAdornment>) }} />
@@ -47,7 +47,7 @@ const VehicleFilterForm = ({ handleFilterSubmit, handleFilterClear }: VehicleFil
         <TextField onChange={handleOperatorNameChange} value={operatorName} label="Operator Name" size="small" fullWidth InputProps={{ startAdornment: (<InputAdornment position="start"> <PersonIcon /> </InputAdornment>) }} />
       </Grid>
       <Grid item xs={12} md={3}>
-        <TextField onChange={handleLocationChange} value={location} label="Location" size="small" fullWidth InputProps={{ startAdornment: (<InputAdornment position="start"> <LocationOnIcon /> </InputAdornment>) }} />
+        <TextField onChange={handleLocationChange} value={route} label="Route" size="small" fullWidth InputProps={{ startAdornment: (<InputAdornment position="start"> <LocationOnIcon /> </InputAdornment>) }} />
       </Grid>
       <Grid item xs={12} md={3}>
         <Button type="submit" fullWidth variant="contained" >Search</Button>
@@ -56,7 +56,7 @@ const VehicleFilterForm = ({ handleFilterSubmit, handleFilterClear }: VehicleFil
         <Button onClick={handleClear}>Clear Filters</Button>
       </Grid>
     </Grid>
-   );
+  );
 }
- 
+
 export default VehicleFilterForm;
