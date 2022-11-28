d0b136568df780192a7711dc855b4fe4ce0d3148
caracarmelencabo <118359117+caracarmelencabo@users.noreply.github.com>
2022-11-28 11:56:47 +0800

    add contactus page

---
+++ arangkada/src/pages/cara/ContactUsPage.tsx
@@ -0,0 +1,15 @@
+
+import ContactUs from "../../components/cara/ContactUs"
+import Footer from "../../components/Footer"
+
+
+
+export default function RegistrationOne() {
+    return (
+      <div>
+        <ContactUs></ContactUs>
+        <Footer name="Cara Carmel Encabo" course="BSIT" section="G2"/>
+      </div>
+      
+    )
+}
