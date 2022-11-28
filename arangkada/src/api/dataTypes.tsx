192bd043796b2387f019d1eba0d980474aea15ef
Faith Rosalijos <faithrosalijos@gmail.com>
2022-11-28 19:57:34 +0800

    Add data types

---
+++ arangkada/src/api/dataTypes.tsx
@@ -0,0 +1,56 @@
+export type Account = {
+  accountId: number,
+  firstname: string,
+  middlename: string,
+  lastname: string,
+  birthdate: string,
+  age: number,
+  contactNumber: string,
+  address: string,
+  gender: string,
+  username: string,
+  password: string,
+  accountType: string,
+}
+
+export type Operator = {
+  operatorId: number,
+  businessName: string,
+  permitNumber: string,
+  account: Account,
+}
+
+export type Driver = {
+  driverId: number,
+  licensenumber: string,
+  licensecode: string,
+  account: Account,
+}
+
+export type Vehicle = {
+  vehicleId: number,
+  plateNumber: string,
+  route: string,
+  vehicleType: string,
+  makeModel: string,
+  vin: number,
+  orStatus: string,
+  vehicleCondition: string,
+  rentalFee: number,
+  operator: Operator,
+}
+
+export type Rental = {
+  rentalId: number,
+  startDate: string,
+  endDate: string,
+  status: string,
+  vehicle: Vehicle,
+  driver: Driver,
+}
+
+export type Payment = {
+  paymentId: number,
+  amount: number,
+  datePaid: string,
+}
