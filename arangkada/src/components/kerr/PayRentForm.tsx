70a801efc32c3c3d0f354a89f51eaa3ea8387773
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-12-02 12:38:08 +0800

    Modify form from DatePicker to TextField

--- arangkada/src/components/kerr/PayRentForm.tsx
+++ arangkada/src/components/kerr/PayRentForm.tsx
@@ -1,46 +1,43 @@
-import { Button, Stack, TextField } from "@mui/material";
-import { DesktopDatePicker } from "@mui/x-date-pickers";
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { Button, Stack, TextField, Grid } from "@mui/material";
 import { useState } from "react";
 
 const PayRentForm = () => {
-    const currentDate = new Date(new Date().setHours(0, 0, 0, 0));
-    const [datePaid, setDatePaid] = useState<Date | null>(null);
-    const [datePaidError, setDatePaidError] = useState<string | null>(null);
+    const [amount, setAmount] = useState("");
+
   
-    const handleDatePaidChange = (date: Date | null) => {
-        setDatePaid(date);
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setAmount(event.target.value);
     }
   
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      setDatePaidError(null);
   
       ///TODO: DatePaid must be set automatically to the current date of today.
-      if (datePaid === null)
-        setDatePaidError("Please input the date of payment.");
-      else
-        console.log(datePaid);
     }
   
-    return (
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Stack spacing={3} component="form" onSubmit={handleSubmit}>
-          <DesktopDatePicker
-            label="Start Date"
-            minDate={currentDate}
-            value={datePaid}
-            onChange={handleDatePaidChange}
-            renderInput={(params) => <TextField {...params} size="small" error={datePaidError !== null} helperText={datePaidError} />}
-          />
+    return ( 
+      <>
+      <Grid container spacing={2} onSubmit={handleSubmit} component="form" sx={{marginTop: 2, marginBottom: 5}}>
+      <Grid item xs={12} md={4}>
+         <TextField 
+              onChange={handleAmountChange} 
+              value={amount} 
+              label="Amount" 
+              size="small"
+              fullWidth 
+              required
+              sx={{margin: 1, }}>
+          </TextField> 
+      </Grid>
+          <Grid item xs={12} >
           <Stack spacing={3} direction={{ xs: "column-reverse", md: "row" }} sx={{ justifyContent: "end" }}>
-            <Button color="secondary" variant="contained" sx={{ width: "250px" }}>Go Back</Button>
-            <Button type="submit" variant="contained" sx={{ width: "250px" }}>Rent Vehicle</Button>
+            <Button color="secondary" variant="contained" sx={{ width: "250px" }}>Cancel</Button>
+            <Button type="submit" variant="contained" sx={{ width: "250px"}}>Add Vehicle</Button>
           </Stack>
-        </Stack>
-      </LocalizationProvider>
-    );
+          </Grid>
+      </Grid>
+      </>
+     );
   }
   
   export default PayRentForm;
