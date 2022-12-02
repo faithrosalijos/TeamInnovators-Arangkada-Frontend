54b486918baa9523830982b5be8fd2e13ede53a1
caracarmelencabo <118359117+caracarmelencabo@users.noreply.github.com>
2022-12-02 14:18:21 +0800

    Remove fontFamily

--- arangkada/src/components/cara/Welcome.tsx
+++ arangkada/src/components/cara/Welcome.tsx
@@ -10,7 +10,7 @@ export default function Welcome(props:MyCompType) {
     return (
         <div>
             <img src={Logo} alt={"arangkada logo"} style={{width: 150, height: 150}}/>
-            <h2 style={{fontSize: '50px', color: '#D2A857', lineHeight: '.2em', fontFamily: 'Inter-Regular'}}>{props.line1}</h2>
+            <h2 style={{fontSize: '50px', color: '#D2A857', lineHeight: '.2em'}}>{props.line1}</h2>
             <h1 style={{fontSize: '80px', color: '#90794C', lineHeight: '.2em'}}>{props.line2}</h1>
             <i><p style={{color: '#90794C'}}>{props.line3}</p></i>
             <br></br>
