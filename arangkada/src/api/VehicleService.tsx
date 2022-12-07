94eaca53903e46c572f438ccf37217da435776e2
Mariel Genodiala <118412577+marielgenodiala@users.noreply.github.com>
2022-12-07 14:33:50 +0800

    edited VehicleService API
    getVehicleByOperatorOperatorId
    getVehicleByVehicleType

--- arangkada/src/api/VehicleService.tsx
+++ arangkada/src/api/VehicleService.tsx
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const PAYMENT_BASE_URL = "http://localhost:8080/vehicle";
+const VEHICLE_BASE_URL = "http://localhost:8080/vehicle";
 
 class VehicleService {
 
-    getVehicleByOperatorId(id: string) {
-      return axios.get(PAYMENT_BASE_URL+"/getVehicleByOperatorId/"+id);
-    }
-  
+  getVehicleByOperatorOperatorId(id: string) {
+    return axios.get(VEHICLE_BASE_URL + "/getByOperatorId/" + id);
   }
-  
-  export default new VehicleService();
+
+  getVehicleByVehicleType(vehicleType: string) {
+    return axios.get(VEHICLE_BASE_URL + "/getByVehicleType/?vehicleType=" + vehicleType);
+  }
+
+
+}
+
+export default new VehicleService();
