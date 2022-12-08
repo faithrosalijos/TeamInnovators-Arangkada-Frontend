e0ba7416d163589f29c603542b6012c5f3d3fb50
Faith Rosalijos <faithrosalijos@gmail.com>
2022-12-08 20:42:33 +0800

    Create PendingRentalContext.tsx

---
+++ arangkada/src/helpers/PendingRentalsContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, Dispatch, useState } from "react";
+import { Rental } from "../api/dataTypes"
+
+export type PendingRentalsContextType = {
+  pendingRentals: Rental[],
+  setPendingRentals: Dispatch<React.SetStateAction<Rental[]>>,
+}
+
+export const PendingRentalsContext = createContext<PendingRentalsContextType | null>(null);
+
+const PendingRentalsContextProvider = (props: { children: React.ReactNode }) => {
+  const [pendingRentals, setPendingRentals] = useState<Rental[]>([]);
+
+  const value = {
+    pendingRentals,
+    setPendingRentals,
+  }
+
+  return (
+    <PendingRentalsContext.Provider value={value}>
+      {props.children}
+    </PendingRentalsContext.Provider>
+  )
+}
+
+export default PendingRentalsContextProvider;
