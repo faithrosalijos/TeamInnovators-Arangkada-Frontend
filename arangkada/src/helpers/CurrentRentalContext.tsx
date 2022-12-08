b9c03aecbcd5a8ec52736e72f635ec5613f58e10
Faith Rosalijos <faithrosalijos@gmail.com>
2022-12-08 20:42:19 +0800

    Create CurrentRentalContext.tsx

---
+++ arangkada/src/helpers/CurrentRentalContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, Dispatch, useState } from "react";
+import { Rental } from "../api/dataTypes"
+
+export type CurrentRentalContextType = {
+  currentRental: Rental,
+  setCurrentRental: Dispatch<React.SetStateAction<Rental>>,
+}
+
+export const CurrentRentalContext = createContext<CurrentRentalContextType | null>(null);
+
+const CurrentRentalContextProvider = (props: { children: React.ReactNode }) => {
+  const [currentRental, setCurrentRental] = useState<Rental>({} as Rental);
+
+  const value = {
+    currentRental,
+    setCurrentRental,
+  }
+
+  return (
+    <CurrentRentalContext.Provider value={value}>
+      {props.children}
+    </CurrentRentalContext.Provider>
+  )
+}
+
+export default CurrentRentalContextProvider;
