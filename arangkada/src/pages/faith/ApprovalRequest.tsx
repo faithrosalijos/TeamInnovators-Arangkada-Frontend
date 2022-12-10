298888b08abd5163922b656492a57a956ca8d5c5
Faith Rosalijos <faithrosalijos@gmail.com>
2022-12-10 12:38:45 +0800

    Add pagination to approval requests

--- arangkada/src/pages/faith/ApprovalRequest.tsx
+++ arangkada/src/pages/faith/ApprovalRequest.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Pagination, Typography } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import PageHeader from "../../components/PageHeader";
@@ -9,10 +9,12 @@ import ResponseError from "../../components/faith/ResponseError";
 import { PendingRentalsContext, PendingRentalsContextType } from "../../helpers/PendingRentalsContext";
 
 const ApprovalRequest = () => {
+  const PAGE_SIZE = 5;
+  const [pagination, setPagination] = useState({from: 0, to: PAGE_SIZE});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const { pendingRentals, setPendingRentals } = useContext(PendingRentalsContext) as PendingRentalsContextType;
-
+  
   useEffect(() => {
     RentalService.getRentalsByOperatorAndStatus("2", "PENDING").then((response) => {
       setPendingRentals(response.data);
@@ -24,6 +26,13 @@ const ApprovalRequest = () => {
     })
   }, [])
 
+  const handlePaginationChange = (page: number) => {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = (page - 1) * PAGE_SIZE + PAGE_SIZE;
+    setPagination({ from: from, to: to });
+    window.scroll(0, 0);
+  }
+
   if (loading) return (<Loading />)
 
   if (error !== '') return (<ResponseError message={error} />)
@@ -33,8 +42,19 @@ const ApprovalRequest = () => {
       <Box mt="12px" display="flex" flexDirection="column" sx={{ minHeight: "80vh" }}>
         <PageHeader title="Approval Requests" />
         <br></br>
-        {pendingRentals.length !== 0 && <RentalCardList rentals={pendingRentals} />}
-        {pendingRentals.length === 0 && <Typography variant="body1" color="text.secondary">No pending request.</Typography>}
+        {
+          pendingRentals.length !== 0?
+            <>
+              <RentalCardList rentals={pendingRentals.slice(pagination.from, pagination.to)} />
+              <br/> 
+              <Pagination 
+                color="primary"
+                count={Math.ceil(pendingRentals.length / PAGE_SIZE)} 
+                onChange={(event, page) => handlePaginationChange(page)}
+              /> 
+            </>:
+            <Typography variant="body1" color="text.secondary">No pending request.</Typography>
+        }
       </Box>
       <Footer name="Faith Rosalijos" course="BSIT" section="G1" />
     </>
