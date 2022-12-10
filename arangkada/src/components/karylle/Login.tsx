34abc942675e4660b9378a47bb7c34c79e0243ba
karyllejaycaballero <karyllejay.caballero@cit.edu>
2022-12-11 00:45:20 +0800

    Modify Login

--- arangkada/src/components/karylle/Login.tsx
+++ arangkada/src/components/karylle/Login.tsx
@@ -1,22 +1,22 @@
 import { Card, Button, TextField } from "@mui/material"
-import Loginbg from '../../images/Login.png';
 import React from "react"
+
 
 
 export default function Login(){
     return (
-        <div className="bgimg">
-            
-            <div style={{ paddingLeft: 50, backgroundColor: 'white', padding: 3, marginLeft: 650, maxWidth: 550, maxHeight: 670, textAlign: 'left', marginBottom:'1rem'}}>
-                
-                <h2>Log in</h2><br></br>
-                <TextField id="outlined-basic" label="Username" variant="outlined" sx={{margin: 1, width: { sm:400, md:400}}}/><br></br><br></br>
-                <TextField id="outlined-basic" label="Password" variant="outlined" sx={{margin: 1, width: { sm:400, md:400}}}/><br></br>
-                <Button variant="contained" style={{backgroundColor: '#D2A857', marginTop:25, paddingInline:40, marginBottom:'45px', textAlign: 'center'}}>Log in</Button>
-                <p style={{color: 'black', fontSize: '15px', marginLeft: '50rem'}}>  <a href="https://www.youtube.com"  className="links">Forgot password?</a></p>
+        <div >
+            <div><br></br><br></br><br></br><br></br></div>
+            <div style={{ paddingLeft: 35, paddingRight: 35, backgroundColor: 'white', padding: 2, marginLeft: -130, maxWidth: 600, maxHeight: 670, textAlign: 'left', marginBottom:'1rem', borderRadius:35, opacity:'0.91'}}>
+                <br></br>
+                <h1>Log in</h1><br></br><br></br>
+                <TextField id="outlined-basic" label="Username" variant="outlined" sx={{margin: 0, width: { sm:400, md:400}}}/><br></br><br></br><br></br>
+                <TextField id="outlined-basic" label="Password" variant="outlined" sx={{margin: 0, width: { sm:400, md:400}}}/><br></br><br></br>
+                <p style={{color: 'brown', fontSize: '15px'}}>  <a href="https://www.youtube.com"  className="links">Forgot password?</a></p><br></br>
+                <Button variant="contained" style={{backgroundColor: '#D2A857', marginLeft: 140, marginTop:25, paddingInline:40, marginBottom:'45px', textAlign: 'center', borderRadius:20}}>Log in</Button>
                 
             </div>
-            <p style={{color: 'white', fontSize: '15px', marginLeft: '50rem'}}>Not registered yet?  <a href="https://www.youtube.com"  className="links">Create an account</a></p>
+            <p style={{color: 'white', fontSize: '15px', marginLeft: '-8rem'}}>Not registered yet?  <a href="https://www.youtube.com"  className="linksL">Create an account</a></p>
         </div>
     )
 }
