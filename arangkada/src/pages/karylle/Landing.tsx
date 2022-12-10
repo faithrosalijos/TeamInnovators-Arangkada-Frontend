a4da4dd8f27e66fd9b4b9a478009649f2feb8dc7
karyllejaycaballero <karyllejay.caballero@cit.edu>
2022-12-10 13:13:26 +0800

    Update LandingPage

--- arangkada/src/pages/karylle/Landing.tsx
+++ arangkada/src/pages/karylle/Landing.tsx
@@ -1,11 +1,13 @@
 import { Box, Toolbar, Typography } from "@mui/material";
 import LandingPage from "../../components/karylle/LandingPage";
 import Footer from "../../components/Footer";
+import UserAppBarB from "../../components/UserAppBarB";
 
 export default function RegistrationOne() {
     return (
         <div>
             <div className="App">
+                <UserAppBarB></UserAppBarB>
                 <div className="pstyle">
                     <Box component="main" sx={{ p: 6 }}>
                         <Toolbar />
