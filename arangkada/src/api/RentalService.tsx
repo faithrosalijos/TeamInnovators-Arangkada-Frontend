39871e2eb6a29420981233b712304463b9f5b210
Faith Rosalijos <faithrosalijos@gmail.com>
2022-12-11 21:50:09 +0800

    Modify postRental method

--- arangkada/src/api/RentalService.tsx
+++ arangkada/src/api/RentalService.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { PostRental, PutRental } from "./dataTypes";
 
 const RENTAL_BASE_URL = "http://localhost:8080/rental";
 
@@ -16,24 +17,11 @@ class RentalService {
     return axios.get(RENTAL_BASE_URL + "/getRentalsByStatusAndVehicleOperatorId/" + id + "?status=" + status);
   }
 
-  postRental(
-    rental: {
-      startDate: string,
-      endDate: string,
-      vehicle: { vehicleId: number }
-      driver: { driverid: number }
-    }) {
+  postRental(rental: PostRental) {
     return axios.post(RENTAL_BASE_URL + "/postRental/", rental);
   }
 
-  putRental(
-    id: string,
-    rental: {
-      startDate: string,
-      endDate: string,
-      status: string,
-      current: boolean
-    }) {
+  putRental(id: string, rental: PutRental) {
     return axios.put(RENTAL_BASE_URL + "/putRental/" + id, rental);
   }
 
