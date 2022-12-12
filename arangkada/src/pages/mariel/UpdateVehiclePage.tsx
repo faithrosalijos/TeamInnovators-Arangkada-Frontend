5541aab797192053ae9f27e9d61c39ee12eab644
Mariel Genodiala <118412577+marielgenodiala@users.noreply.github.com>
2022-12-13 06:55:01 +0800

    Update UpdateVehiclePage

--- arangkada/src/pages/mariel/UpdateVehiclePage.tsx
+++ arangkada/src/pages/mariel/UpdateVehiclePage.tsx
@@ -1,5 +1,4 @@
 import { Box, Divider } from "@mui/material";
-import { useState } from "react";
 import Footer from "../../components/Footer";
 import ViewVehicle from "../../components/mariel/ViewVehicle";
 import PageHeader from "../../components/PageHeader";
@@ -11,8 +10,7 @@ const MyVehicles = () => {
   return ( 
     <Box sx={{ padding: "12px 0 0" }}>
       <PageHeader title="Update Vehicle Information"/>
-      <ViewVehicle operatorID={"Operator1"} {...{ id: 1, plateNumber: "ABC123", route: "01K", vehicleType: "Jeepney", makeModel: "1995-Jeep", vin: "GXXXX XXX XXXXX", rentalFee: 150.00, orStatus: "Updated", condition: "Ready To Use" }}/>
-      {/* <UpdateVehicle { id: 1, plateNumber: "ABC123", route: "01K", vehicleType: "Jeepney", makeModel: "1995-Jeep", vin: "GXXXX XXX XXXXX", rentalFee: 150.00, orStatus: "Updated", condition: "Ready To Use" }/> */}
+      <ViewVehicle/>
       <Divider/>
       <Footer name="Mariel Genodiala" course="BSIT" section="G3"/>
     </Box>
