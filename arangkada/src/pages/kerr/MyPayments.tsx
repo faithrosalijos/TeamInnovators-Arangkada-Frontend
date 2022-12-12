30bbe7892709146e80e26b48632f74a347aff8d9
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-12-12 10:55:17 +0800

    Add button handler for pay rent button

--- arangkada/src/pages/kerr/MyPayments.tsx
+++ arangkada/src/pages/kerr/MyPayments.tsx
@@ -4,10 +4,15 @@ import { Payment } from "../../api/dataTypes";
 import PageHeader from "../../components/PageHeader";
 import Footer from "../../components/Footer";
 import PaymentService from "../../api/PaymentService";
+import { useNavigate } from "react-router-dom";
 
 const MyPayments = () => {
+    const navigate = useNavigate();
     const [currentPayment, setCurrentPayment] = useState<Payment>();
-  
+
+    const handlePayRentClick = () => {
+      navigate("/driver/payments/payrent/");
+  }
     useEffect(() => {
       PaymentService.getPaymentsByDriverId("2").then((response) => {
         setCurrentPayment(response.data)
@@ -24,6 +29,7 @@ const MyPayments = () => {
             <Grid item xs={60} md={50}>
               <Button 
                 id="AddBtn"
+                onClick={handlePayRentClick}
                 type="submit" 
                 variant="contained"  
                 sx={{marginLeft:160, height: "65px",width: "65px",borderRadius: "100px", marginTop: 2,marginRight: 2, marginBottom: 4}}>
