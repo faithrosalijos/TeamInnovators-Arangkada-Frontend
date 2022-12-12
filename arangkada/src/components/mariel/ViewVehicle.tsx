f19779af16fe90bcb9a87d7c402ecfb195468588
Mariel Genodiala <118412577+marielgenodiala@users.noreply.github.com>
2022-12-13 06:53:16 +0800

    Update ViewVehicle

--- arangkada/src/components/mariel/ViewVehicle.tsx
+++ arangkada/src/components/mariel/ViewVehicle.tsx
@@ -1,192 +1,198 @@
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Button, FormControl, Grid,  InputLabel, MenuItem, Select, SelectChangeEvent, Stack, TextField, } from "@mui/material";
-import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import VehicleService from "../../api/VehicleService";
+import axios from "axios";
 
-
-type MyVehicleType ={
-    id: number,
-    plateNumber: string,
-    route: string,
-    vehicleType: string,
-    makeModel: string,
-    vin: string,
-    orStatus: string,
-    condition:string,
-    operatorID : string,
-    rentalFee: number,
-    driverName?: string,
-    vehicleStatus?:string,
-
-}
-
-const ViewandUpdateVehicle  = (props:MyVehicleType) =>{
-    const [route, setRoute] = useState("");
-    const [renatlFee, setRentalFee] = useState("");
-    const [driverName, setDriverName] = useState("");
-    const [orStatus, setOrStatus] = React.useState('');
-    const [condition, setCondition] = React.useState('');
-    const [vehicleStatus, setVehicleStatus] = React.useState('');
-
-    const handleOrStatusChange = (event: SelectChangeEvent) => {
-        setOrStatus(event.target.value as string);
-    }
-    const handleConditionChange = (event: SelectChangeEvent) => {
-        setCondition(event.target.value as string);
-    }
-    const handleVehicleStatusChange = (event: SelectChangeEvent) => {
-        setVehicleStatus(event.target.value as string);
-    }
-    const handleRouteChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRoute(event.target.value);
-    }
+const ViewandUpdateVehicle  = () =>{
     
-    const handleRentalFeeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRentalFee(event.target.value);
-    }
-    const handleDriverNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRoute(event.target.value);
-    }
-    const handleSubmit = (event: React.SyntheticEvent) => {
-        event.preventDefault();
-    }
-    const handleClear = (event: React.MouseEvent) => {
-        setRoute("");
-        setRentalFee("");
-        setDriverName("");
-    }
-   
+    const navigate = useNavigate();
+    const para = useParams() as { id: string };
 
-  return ( 
-    <>
-    <Grid container spacing={2} onSubmit={handleSubmit} component="form" sx={{marginTop: 2,  marginBottom: 5}}>
-    <Grid item xs={12} md={4}>
-       <TextField 
-            disabled
-            value={props.plateNumber} 
-            label="Plate Number" 
-            size="small"
-            variant="outlined" 
-            fullWidth
-            sx={{margin: 1}}>
-        </TextField> 
-    </Grid>
-    <Grid item xs={12} md={4}>
-       <TextField 
-            onChange={handleRouteChange} 
-            value={props.route} 
-            label="Route" 
-            size="small" 
-            variant="outlined" 
-            fullWidth
-            sx={{margin: 1, }}> 
-        </TextField>
-    </Grid>
-    <Grid item xs={12} md={4}>
-       <TextField 
-            disabled
-            value={props.vehicleType}
-            label="Vehicle Type" 
-            size="small" 
-            variant="outlined" 
-            fullWidth
-            sx={{margin: 1,}} >
-       </TextField>
-    </Grid>
-    <Grid item xs={12} md={6}>
-        <TextField 
-            disabled
-            value={props.vin}
-            label="VIN" 
-            size="small" 
-            variant="outlined" 
-            fullWidth
-            sx={{margin: 1,}}>
-        </TextField>
-    </Grid>
-    <Grid item xs={12} md={6}>
-        <FormControl fullWidth >
-            <InputLabel id="demo-simple-select-label" sx={{margin: 1}}>Vehicle Status</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={props.vehicleStatus}
-                    label="Vehicle Status"
-                    size="small"
-                    onChange={handleConditionChange}
-                    sx={{margin: 1}}>
-                        <MenuItem value={'Still on the Road'}>Rented</MenuItem>
-                        <MenuItem value={'Available'}>Available</MenuItem> 
-                {/* {myVehicle.condition} */}
-                </Select>  
-        </FormControl> 
-    </Grid>
-    <Grid item xs={12} md={6}>
-      <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label" sx={{margin: 1}}>OR, CR Status</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={props.orStatus}
-                    label="OR, CR Status"
-                    size="small"
-                    onChange={handleOrStatusChange}
-                    sx={{margin: 1}}>
-                        <MenuItem value={'Updated'}>Updated</MenuItem>
-                        <MenuItem value={'Renewed'}>Renewed</MenuItem>
-                        <MenuItem value={'Expired'}>Expired</MenuItem> 
-                {/* {myVehicle.orStatus} */}
-                </Select>  
-        </FormControl> 
-    </Grid>
-    <Grid item xs={12} md={6}>
-        <FormControl fullWidth >
-            <InputLabel id="demo-simple-select-label" sx={{margin: 1}}>Vehicle Condition</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={props.condition}
-                    label="Vehicle Condition"
-                    size="small"
-                    onChange={handleConditionChange}
-                    sx={{margin: 1}}>
-                        <MenuItem value={'Ready To Use'}>Ready To Use</MenuItem>
-                        <MenuItem value={'Not Ready To Use'}>Not Ready To</MenuItem> 
-                {/* {myVehicle.condition} */}
-                </Select>  
-        </FormControl> 
+    const [vehicle, setVehicle]= useState({
+        plateNumber: "",
+        route: "",
+        vehicleType: "",
+        makeModel: "",
+        vin: "",
+        orStatus: "",
+        vehicleCondition: "",
+        rentalFee: "",
+        rented: false,
+    })
+    const { plateNumber, route, vehicleType, makeModel,vin,orStatus,vehicleCondition,rentalFee, rented} = vehicle;
+    const updateVehicle = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        await axios.put(`http://localhost:8080/vehicle/putVehicle/${para.id}`,{
+        // VehicleService.putVehicle( {
+        route:vehicle.route,
+        rentalFee:Number(vehicle.rentalFee),
+        orStatus:vehicle.orStatus,
+        vehicleCondition:vehicle.vehicleCondition,
+    })
+    .then((res:any)=> console.log('Editing Data'))
+    .catch((err:string) => console.log(err))
+    // alert("Changes Successfully change")
+    // navigate("/operator/view-vehicle")
+    };
+
+  useEffect(() => {
+    // console.log(isRented)
+    VehicleService.getVehicleByVehicleId(para.id).then((response) => {
+      setVehicle(response.data);
+    //   console.log(response.data)
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, []);
+     
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setVehicle({ ...vehicle, [e.target.name]: e.target.value });
+        //console.log(e.target.name)
+    };
+    const handleSelectChange = (event: SelectChangeEvent) => {
+        setVehicle({ ...vehicle, [event.target.name]: event.target.value });
+    }
+
+    return ( 
+        <>
+        <Grid container spacing={2} onSubmit={updateVehicle}component="form" sx={{marginTop: 2, marginBottom: 5}}>
+        <Grid item xs={12} md={4}>
+           <TextField 
+                onChange={handleChange} 
+                disabled
+                value={plateNumber} 
+                name="plateNumber"
+                label="Plate Number" 
+                size="small"
+                fullWidth 
+                sx={{margin: 1, }}>
+            </TextField> 
+        </Grid>
+        <Grid item xs={12} md={4}>
+           <TextField 
+                onChange={handleChange} 
+                value={route} 
+                name="route"
+                label="Route" 
+                size="small" 
+                fullWidth 
+                required
+                sx={{margin: 1,  }}> 
+            </TextField>
+        </Grid>
+        <Grid item xs={12} md={4}>
+           <TextField 
+                onChange={handleChange} 
+                value={vehicleType}
+                name="vehicleType"
+                disabled
+                label="Vehicle Type" 
+                size="small" 
+                fullWidth 
+                sx={{margin: 1, }} >
+           </TextField>
         </Grid>
         <Grid item xs={12} md={6}>
             <TextField 
+                onChange={handleChange} 
+                value={vin}
+                label="VIN" 
+                name="vin"
                 disabled
-                value={props.makeModel}
-                label="Make and Model" 
                 size="small" 
                 variant="outlined" 
                 fullWidth
                 sx={{margin: 1, }}>
             </TextField>
-        </Grid>
+            </Grid>
         <Grid item xs={12} md={6}>
             <TextField 
-                onChange={handleRentalFeeChange} 
-                value={props.rentalFee}
-                contentEditable
-                label="Rental Fee" 
+
+                value={rented===false?"Not Rented" : "Rented"}
+                disabled
+                onChange={handleChange} 
+                label="Rental Status" 
                 size="small" 
-                variant="outlined"
+                name="rented"
                 fullWidth
+                variant="outlined" 
                 sx={{margin: 1,}}>
             </TextField>
         </Grid>
-
-     <Grid item xs={12} >
-        <Stack spacing={3} direction={{ xs: "column-reverse", md: "row" }} sx={{ justifyContent: "end" }}>
-          <Button color="secondary" variant="contained" sx={{ width: "250px" }}>Cancel</Button>
-          <Button type="submit" variant="contained" sx={{ width: "250px"}}>Save Changes</Button>
-        </Stack>
+        <Grid item xs={12} md={6}>
+          <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label" >OR, CR Status</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={orStatus}
+                        label="OR, CR Status"
+                        required
+                        name="orStatus"
+                        size="small" 
+                        onChange={handleSelectChange}
+                        sx={{margin: 1}}>
+                            <MenuItem value={'Updated'}>Updated</MenuItem>
+                            <MenuItem value={'Renewed'}>Renewed</MenuItem>
+                            <MenuItem value={'Expired'}>Expired</MenuItem> 
+                    {/* {myVehicle.orStatus} */}
+                    </Select>  
+            </FormControl> 
         </Grid>
-    </Grid>
-    </>
-   );
-}
+        <Grid item xs={12} md={6}>
+            <FormControl fullWidth >
+                <InputLabel id="demo-simple-select-label" >Vehicle Condition</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={vehicleCondition}
+                        label="Vehicle Condition"
+                        required
+                        name="vehicleCondition"
+                        size="small" 
+                        onChange={handleSelectChange}
+                        sx={{margin: 1}}>
+                            <MenuItem value={'Ready to use'}>Ready To Use</MenuItem>
+                            <MenuItem value={'Not Ready To Use'}>Not Ready To Use</MenuItem> 
+                    {/* {myVehicle.condition} */}
+                    </Select>  
+            </FormControl> 
+        </Grid>
+            <Grid item xs={12} md={6}>
+                <TextField 
+                    onChange={handleChange} 
+                    value={makeModel}label="Make and Model" 
+                    size="small" 
+                    variant="outlined" 
+                    disabled
+                    name="makeModel"
+                    fullWidth
+                    sx={{margin: 1,  height:"8vh"}}>
+                </TextField>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                <TextField 
+                    onChange={handleChange} 
+                    value={rentalFee}
+                    label="Rental Fee" 
+                    size="small" 
+                    variant="outlined"
+                    name="rentalFee"
+                    required 
+                    fullWidth
+                    sx={{margin: 1, marginRight:25, height:"8vh" }}>
+                </TextField>
+            </Grid>
+            <Grid item xs={12} >
+            <Stack spacing={3} direction={{ xs: "column-reverse", md: "row" }} sx={{ justifyContent: "end" }}>
+              <Button  onClick={() => navigate(-1)} color="secondary" variant="contained" sx={{ width: "250px" }}>Cancel</Button>
+              <Button  type="submit" variant="contained" sx={{ width: "250px"}}>Save Changes</Button> 
+              {/* onClick={(e)=>{e.preventDefault(); console.log(data)}} */}
+            </Stack>
+            </Grid>
+        </Grid>
+        </>
+       );
+    }
 export default ViewandUpdateVehicle;
