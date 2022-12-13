21db0910c31fdd34e32671bf290993e161b53e4b
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-12-13 21:56:48 +0800

    Add PaymentCard

---
+++ arangkada/src/components/kerr/PaymentCard.tsx
@@ -0,0 +1,4 @@
+import { Payment } from "../../api/dataTypes";
+import PaymentService from "../../api/RentalService";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
