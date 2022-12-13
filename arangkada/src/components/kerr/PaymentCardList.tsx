4cf826c0e38d5f4bbe60206890d5f75c406e31d5
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-12-13 22:19:56 +0800

    Implement PaymentCardList

--- arangkada/src/components/kerr/PaymentCardList.tsx
+++ arangkada/src/components/kerr/PaymentCardList.tsx
@@ -1,3 +1,23 @@
 import { Grid } from "@mui/material";
 import { Payment } from "../../api/dataTypes";
 import PaymentCard from "./PaymentCard";
+
+type MyPaymentCardListProps = {
+    myPayment: Payment[],
+  }
+
+  const PaymentCardList = ({ myPayment }: MyPaymentCardListProps) => {
+    return ( 
+      <Grid container spacing={2} sx={{ padding: "12px 0" }}>
+        {myPayment.map((myPayment) => (
+          <Grid xs={12} md={12} lg={12} item key={myPayment.paymentId}>
+            <PaymentCard myPayment={myPayment} />
+          </Grid>
+        ))}
+        
+      </Grid>
+      
+     );
+  }
+
+export default PaymentCardList;
