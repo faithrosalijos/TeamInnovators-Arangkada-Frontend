37af097794cd28c3e69fa58a73e203ff4b294f37
karyllejaycaballero <karyllejay.caballero@cit.edu>
2022-12-14 19:37:20 +0800

    modify registerdriver

--- arangkada/src/pages/karylle/RegisterDriver1.tsx
+++ arangkada/src/pages/karylle/RegisterDriver1.tsx
@@ -1,26 +1,25 @@
 import { Box, Toolbar, Typography } from "@mui/material";
-import Registration from "../../components/karylle/RegistrationFormD";
+import Registration from "../../components/karylle/RegistrationDriver";
 import Welcome from "../../components/cara/Welcome"
 import Footer from "../../components/Footer";
+import UserAppbar from "../../components/UserAppbar";
 
 
 export default function RegistrationOne() {
     return (
+      <div>
+      <UserAppbar></UserAppbar>
       <div className="App">
-        <div className="bstyle">
-          <div className='contain'>
-            <div className='wrapper'>
-              <div className='left'>
-                <Welcome line1='Welcome to' line2='Arangkada' line3='We get you moving!'></Welcome>
-              </div>
-            <div className='right'>
-              <Registration></Registration>
-            </div>
-            </div>
+          <div className="bstyle">
+              <Box component="main" sx={{ p: 6 }}>
+                  <Toolbar />
+                  <Typography>
+                      <Registration></Registration>
+                  </Typography>
+              </Box>
+              <Footer name="Cara Carmel Encabo" course="BSIT" section="G2"/>
           </div>
-          <Footer name="Cara Carmel Encabo" course="BSIT" section="G2"/>
-        </div>
       </div>
-      
-    )
+  </div>
+)
 }
