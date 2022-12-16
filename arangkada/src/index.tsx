8151121ac26e44c1b3303b973da8685d10eb4e74
Faith Rosalijos <faithrosalijos@gmail.com>
2022-12-16 11:16:57 +0800

    Add user context provider

--- arangkada/src/index.tsx
+++ arangkada/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import UserContextProvider from './helpers/UserContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider> 
     </BrowserRouter>
   </React.StrictMode>
 );
