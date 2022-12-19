ccf0ab00f12da23afa50701c415d819de1e77984
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-12-19 12:39:14 +0800

    Implement Transactions

--- arangkada/src/pages/kerr/Transactions.tsx
+++ arangkada/src/pages/kerr/Transactions.tsx
@@ -13,7 +13,42 @@ import { UserContext, UserContextType } from "../../helpers/UserContext";
 import { SnackbarContext, SnackbarContextType } from "../../helpers/SnackbarContext";
 
 const Transactions = () => {
+    const { handleSetMessage } = useContext(SnackbarContext) as SnackbarContextType;
+    const { user } = useContext(UserContext) as UserContextType;
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const [payments, setPayments] = useState<Payment[]>([]);
 
+    useEffect(() => {
+        if (user !== null) {
+          PaymentService.getAllPayments()
+          .then((response) => {
+            setPayments(response.data);
+            setError("");
+          }).catch((error) => {
+            handleSetMessage(error.message);
+          }).finally(() => {
+            setLoading(false);
+          })
+        }
+      }, []);
+
+    if (loading) return (<Loading />)
+
+    if (error !== "") return (<ResponseError message={error} />)
+
+    return (
+        <>
+        <Box mt="12px" display="flex" flexDirection="column" sx={{ minHeight: "80vh" }}>
+          <PageHeader title="My Driver's Transactions" />
+          <br></br>
+            {payments.length !== 0 && <PaymentCardList myPayments={payments} />}
+            {payments.length === 0 && <Typography variant="body1" color="text.secondary">No drivers has paid any rents yet.</Typography>}
+            <Footer name="Kerr Labajo" course="BSCS" section="F1" />
+        </Box>
+        </>
+        );
 }
 
 export default Transactions;
